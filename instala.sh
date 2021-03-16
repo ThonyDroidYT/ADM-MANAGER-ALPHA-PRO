bae6f9359f31fe3951d35e930c2d3f45917609ab
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2021-03-16 12:22:23 -0500

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -36,7 +36,7 @@ if [[ "$addnick" = "s" ]] || [[ "$addnick" = "S" ]]; then
 echo -e "${cor[5]} $(source trans -b es:${id} "Ingrese su Apodo de 06-08 Caracteres Recomendados")"
 read -p " Nick: 》" nick
 touch /etc/adm-lite/nick
-echo "${nick}" >> /etc/adm-lite/nick
+echo "$nick" > /etc/adm-lite/nick
 chmod 777 /etc/adm-lite/nick
 echo -e "${cor[4]} $(source trans -b es:${id} "Apodo $(cat /etc/adm-lite/nick) correctamente agregado")!\033[0m"
 elif [[ "$addnick" = "n" ]] || [[ "$addnick" = "N" ]]; then
